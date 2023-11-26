bb138c277f53345c2906e41972b20e7641d71226
Heso <admin@snezik.cz>
2023-11-26 18:59:23 +0100

    Ukládání přestupků do DB.yml

--- src/main/java/cz/hesovodoupe/policerp/PoliceRP.kt
+++ src/main/java/cz/hesovodoupe/policerp/PoliceRP.kt
@@ -3,7 +3,10 @@ package cz.hesovodoupe.policerp
 import cz.hesovodoupe.policerp.commands.nabor
 import cz.hesovodoupe.policerp.commands.prestupek
 import cz.hesovodoupe.policerp.commands.vyhazov
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 public val databaze = mutableMapOf<String, Int>()
 
@@ -16,15 +19,15 @@ object ConfigManager {
     var casJail: String = ""
     var jailName: String = ""
 }
-
 class PoliceRP : JavaPlugin() {
-
+    val f: File = File(dataFolder, "prestupky_db.yml")
+    val cfg = YamlConfiguration.loadConfiguration(f)
     override fun onEnable() {
-        getLogger().info("Testovací zpráva! ")
         println("Děkujeme, že využíváte náš jednoduchý trestací systém pro PoliceRP")
         println("Upozorňujeme, že policisté musí mít právo na PoliceRP.punish")
 
         saveDefaultConfig()
+        loadDbprestupky()
         ConfigManager.emsHireValue = config.getString("nabor.ems").toString()
         ConfigManager.pdHireValue = config.getString("nabor.pd").toString()
         ConfigManager.fireHireValue = config.getString("nabor.fire").toString()
@@ -37,13 +40,11 @@ class PoliceRP : JavaPlugin() {
 
         registerCommands()
 
-        config.getString("ems")
-
     }
 
     override fun onDisable() {
         getLogger().info("Plugin se vypíná")
-
+        saveDbprestupky()
     }
 
     private fun registerCommands() {
@@ -54,6 +55,37 @@ class PoliceRP : JavaPlugin() {
 
 
     }
+
+    fun saveDbprestupky(){
+        for ((key, value) in databaze) {
+            // Zde můžete provádět operace pro každou položku
+            cfg.set(key, value)
+        }
+        cfg.save(f)
+        getLogger().info("prestupky_db Saved")
+    }
+
+    fun loadDbprestupky() {
+        // Načtení konfiguračního souboru
+        cfg.load(f)
+
+        // Vyčištění stávající databáze
+        databaze.clear()
+
+        // Iterace přes všechny klíče v konfiguraci
+        for (key in cfg.getKeys(false)) {
+            // Získání hodnoty pro daný klíč
+            val value = cfg.getInt(key)
+
+            // Aktualizace databáze
+            databaze[key] = value
+        }
+
+        getLogger().info("DB Loaded")
+    }
+
+
+
 
 
 
